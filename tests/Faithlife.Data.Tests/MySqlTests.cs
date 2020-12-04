359528de951dcb3a17a58a6e918b0271bdee23c0
Ed Ball <ed.ball@faithlife.com>
2020-12-03 19:43:35 -0800

    Use unique table name in test.

--- tests/Faithlife.Data.Tests/MySqlTests.cs
+++ tests/Faithlife.Data.Tests/MySqlTests.cs
@@ -1,3 +1,4 @@
+using Faithlife.Data.SqlFormatting;
 using FluentAssertions;
 using MySqlConnector;
 using NUnit.Framework;
@@ -10,16 +11,18 @@ namespace Faithlife.Data.Tests
 		[Test, Explicit("Requires 'docker-compose up' from '/docker'.")]
 		public void PrepareCacheTests()
 		{
-			using var connector = CreateConnector();
-			connector.Command("drop table if exists Items;").Execute();
-			connector.Command("create table Items (ItemId int not null auto_increment primary key, Name varchar(100) not null);").Execute();
+			var tableName = Sql.Raw(nameof(PrepareCacheTests));
 
-			var insertSql = "insert into Items (Name) values (@itemA); insert into Items (Name) values (@itemB);";
+			using var connector = CreateConnector();
+			connector.Command(Sql.Format($"drop table if exists {tableName};")).Execute();
+			connector.Command(Sql.Format($"create table {tableName} (Id int not null auto_increment primary key, Name varchar(100) not null);")).Execute();
+
+			var insertSql = Sql.Format($"insert into {tableName} (Name) values (@itemA); insert into {tableName} (Name) values (@itemB);");
 			connector.Command(insertSql, ("itemA", "one"), ("itemB", "two")).Prepare().Cache().Execute().Should().Be(2);
 			connector.Command(insertSql, ("itemA", "three"), ("itemB", "four")).Prepare().Cache().Execute().Should().Be(2);
 			connector.Command(insertSql, ("itemB", "six"), ("itemA", "five")).Prepare().Cache().Execute().Should().Be(2);
 
-			connector.Command("select Name from Items order by ItemId;").Query<string>().Should().Equal("one", "two", "three", "four", "five", "six");
+			connector.Command(Sql.Format($"select Name from {tableName} order by Id;")).Query<string>().Should().Equal("one", "two", "three", "four", "five", "six");
 		}
 
 		private static DbConnector CreateConnector() => DbConnector.Create(
