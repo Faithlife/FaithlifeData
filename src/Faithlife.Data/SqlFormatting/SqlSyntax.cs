5c85b2d02872b087776b4632365135d84940cab5
Ed Ball <ed.ball@faithlife.com>
2020-12-07 20:37:43 -0800

    Make it easier and safer to create custom SqlSyntax.

--- src/Faithlife.Data/SqlFormatting/SqlSyntax.cs
+++ src/Faithlife.Data/SqlFormatting/SqlSyntax.cs
@@ -12,32 +12,32 @@ namespace Faithlife.Data.SqlFormatting
 		/// </summary>
 		/// <remarks>The default syntax does not support quoted identifiers, since the syntax
 		/// is highly dependent on the type of database and its settings.</remarks>
-		public static SqlSyntax Default { get; } = new DefaultSqlSyntax(nameQuote: null);
+		public static SqlSyntax Default { get; } = new DefaultSqlSyntax();
 
 		/// <summary>
 		/// The syntax for MySQL.
 		/// </summary>
-		public static SqlSyntax MySql { get; } = new DefaultSqlSyntax(nameQuote: '`');
+		public static SqlSyntax MySql { get; } = new SpecificSqlSyntax(nameQuote: '`');
 
 		/// <summary>
 		/// The syntax for PostgreSQL.
 		/// </summary>
-		public static SqlSyntax Postgres { get; } = new DefaultSqlSyntax(nameQuote: '"');
+		public static SqlSyntax Postgres { get; } = new SpecificSqlSyntax(nameQuote: '"');
 
 		/// <summary>
 		/// The syntax for Microsoft SQL Server.
 		/// </summary>
-		public static SqlSyntax SqlServer { get; } = new DefaultSqlSyntax(nameQuote: '[');
+		public static SqlSyntax SqlServer { get; } = new SpecificSqlSyntax(nameQuote: '[');
 
 		/// <summary>
 		/// The syntax for SQLite.
 		/// </summary>
-		public static SqlSyntax Sqlite { get; } = new DefaultSqlSyntax(nameQuote: '"');
+		public static SqlSyntax Sqlite { get; } = new SpecificSqlSyntax(nameQuote: '"');
 
 		/// <summary>
 		/// The prefix for named parameters.
 		/// </summary>
-		public abstract char ParameterPrefix { get; }
+		public virtual char ParameterPrefix => '@';
 
 		/// <summary>
 		/// Escapes a fragment of a LIKE pattern.
@@ -46,13 +46,21 @@ namespace Faithlife.Data.SqlFormatting
 		/// and <c>_</c> escaped as needed. This string is not raw SQL, but rather
 		/// a fragment of a LIKE pattern that should be concatenated with the rest of
 		/// the LIKE pattern and sent to the database via a string parameter.</returns>
-		public abstract string EscapeLikeFragment(string fragment);
+		public virtual string EscapeLikeFragment(string fragment)
+		{
+			const string escapeString = @"\";
+			return (fragment ?? throw new ArgumentNullException(nameof(fragment)))
+				.Replace(escapeString, escapeString + escapeString)
+				.Replace("%", escapeString + "%")
+				.Replace("_", escapeString + "_");
+		}
 
 		/// <summary>
 		/// Quotes the specified identifier so that it can be used as a schema/table/column name
 		/// even if it matches a keyword or has special characters.
 		/// </summary>
-		public abstract string QuoteName(string name);
+		public virtual string QuoteName(string name) =>
+			throw new InvalidOperationException("The default SqlSyntax does not support quoted identifiers. Use a SqlSyntax that matches your database.");
 
 		/// <summary>
 		/// Renders SQL as text and parameters.
@@ -66,39 +74,27 @@ namespace Faithlife.Data.SqlFormatting
 
 		private sealed class DefaultSqlSyntax : SqlSyntax
 		{
-			public DefaultSqlSyntax(char? nameQuote)
+		}
+
+		private sealed class SpecificSqlSyntax : SqlSyntax
+		{
+			public SpecificSqlSyntax(char nameQuote)
 			{
 				(m_nameQuoteStart, m_nameQuoteEnd, m_nameQuoteEndEscape) = nameQuote switch
 				{
 					'"' => ("\"", "\"", "\"\""),
 					'`' => ("`", "`", "``"),
 					'[' => ("[", "]", "]]"),
-					null => default,
 					_ => throw new InvalidOperationException(),
 				};
 			}
 
-			public override char ParameterPrefix => '@';
+			public override string QuoteName(string name) =>
+				m_nameQuoteStart + name.Replace(m_nameQuoteEnd!, m_nameQuoteEndEscape!) + m_nameQuoteEnd;
 
-			public override string EscapeLikeFragment(string fragment)
-			{
-				const string escapeString = @"\";
-				return (fragment ?? throw new ArgumentNullException(nameof(fragment)))
-					.Replace(escapeString, escapeString + escapeString)
-					.Replace("%", escapeString + "%")
-					.Replace("_", escapeString + "_");
-			}
-
-			public override string QuoteName(string name)
-			{
-				if (m_nameQuoteStart is null)
-					throw new InvalidOperationException("The default SqlSyntax does not support quoted identifiers. Use a SqlSyntax that matches your database.");
-				return m_nameQuoteStart + name.Replace(m_nameQuoteEnd!, m_nameQuoteEndEscape!) + m_nameQuoteEnd;
-			}
-
-			private readonly string? m_nameQuoteStart;
-			private readonly string? m_nameQuoteEnd;
-			private readonly string? m_nameQuoteEndEscape;
+			private readonly string m_nameQuoteStart;
+			private readonly string m_nameQuoteEnd;
+			private readonly string m_nameQuoteEndEscape;
 		}
 	}
 }
