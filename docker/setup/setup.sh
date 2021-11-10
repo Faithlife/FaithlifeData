148dae64764ded34a465a7392b8f591ac3c7a29a
Ed Ball <ed.ball@faithlife.com>
2021-11-10 12:45:35 -0800

    Improve docker setup.sh.

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo 'setup.sh'
-docker exec faithlifedata_mssql_1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -Q "create database test;"
-docker exec faithlifedata_mysql_1 mysql -uroot -ptest -e "create schema test collate utf8mb4_bin;"
-docker exec -e PGPASSWORD=test faithlifedata_postgres_1 psql -U root -c "CREATE DATABASE test;"
+echo 'setup.sh: create mssql test database'
+docker exec faithlifedata-mssql-1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -Q "create database test;"
+echo 'setup.sh: create mysql test database'
+docker exec faithlifedata-mysql-1 mysql -uroot -ptest -e "create schema test collate utf8mb4_bin;"
+echo 'setup.sh: create postgres test database'
+docker exec -e PGPASSWORD=test faithlifedata-postgres-1 psql -U root -c "CREATE DATABASE test;"
 echo 'done'
